a13e84b7091874cfb347f4648d562840673ed966
clavyll ryo <152686067+lngndryl@users.noreply.github.com>
2025-09-18 21:27:52 +0800

    Flutter Bottom Navigation

---
+++ lib/pages/contact_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ContactPage '),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+    );
+  }
+}
---
+++ lib/pages/projects_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProjectsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Portfolio '),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+    );
+  }
+}
